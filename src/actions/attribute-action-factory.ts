2b7ad6d2cbbb6fa16dd7406bd112a0506230c481
Adi Marcus <adi.marcus314@gmail.com>
2017-06-28 02:20:44 +0300

    play with actions

--- src/actions/attribute-action-factory.ts
+++ src/actions/attribute-action-factory.ts
@@ -1,4 +1,5 @@
 import {strEnum} from "../helpers/str-enum";
+import {Action} from "redux";
 
 export const AttributeActionTypesConsts = strEnum([
     "SET_PRIMARY_ATTRIBUTE",
@@ -6,33 +7,40 @@ export const AttributeActionTypesConsts = strEnum([
     "INITIALIZE_ATTRIBUTES"
 ]);
 
-export type AttributeActionTypes = keyof typeof AttributeActionTypesConsts;
+//export type AttributeActionTypes = keyof typeof AttributeActionTypesConsts;
 
 
-export type AttributeAction = {
+export type AttributeAction =  SetPrimaryAttributeAction | SetDerivedAttributeAction | InitializeAttributesAction;
+
+
+export type SetPrimaryAttributeAction  = {
     type: "SET_PRIMARY_ATTRIBUTE",
     attributeId: string,
     value: number,
-} | {
+};
+
+export type SetDerivedAttributeAction = {
     type: "SET_DERIVED_ATTRIBUTE",
     attributeId: string,
     cost: number
-} | {
+};
+
+export type InitializeAttributesAction = {
     type: "INITIALIZE_ATTRIBUTES"
-}
+};
 
 
-export const setPrimaryAttribute = (attributeId:string, value: number): AttributeAction => ({
+export const setPrimaryAttribute = (attributeId:string, value: number): SetPrimaryAttributeAction => ({
     type: AttributeActionTypesConsts.SET_PRIMARY_ATTRIBUTE,
     attributeId: attributeId,
     value: value,
-})
+});
 
-export const setDerivedAttribute = (attributeId:string, cost: number): AttributeAction => ({
+export const setDerivedAttribute = (attributeId:string, cost: number): SetDerivedAttributeAction => ({
     type: AttributeActionTypesConsts.SET_DERIVED_ATTRIBUTE,
     attributeId: attributeId,
     cost: cost,
-})
+});
 
 
 export const initializeAttribute = (): AttributeAction => ({

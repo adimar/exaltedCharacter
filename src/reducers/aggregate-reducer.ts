1725fd01937720c791ad0c63872962943ecf8a26
Adi Marcus <adi.marcus314@gmail.com>
2017-06-29 09:57:25 +0300

    attempt at reducer registering itself and not pre written

--- src/reducers/aggregate-reducer.ts
+++ src/reducers/aggregate-reducer.ts
@@ -1,18 +1,40 @@
 
 
 
-import {Action, combineReducers} from "redux";
+import {Action, combineReducers, Store} from "redux";
 import {AttributeReducer} from "./attribute-reducer";
 import {GurpsDataStore, GurpsDataStoreInitialState} from "../datastore/gurps-char-gen-datastore";
 import * as _ from "lodash";
 
-const reducersArray = [AttributeReducer];
+
 
 export  const AggregateReducer = <A extends Action>(state: GurpsDataStore = GurpsDataStoreInitialState, action: A): GurpsDataStore => {
     console.log("AggregateReducers."+action.type+"("+JSON.stringify(action)+")");
-    _.forEach(reducersArray,reducer=>{
+    _.forEach(ReducerRegistry.reducers,reducer=>{
         state = reducer(state,<any>action);
     });
 
     return state;
 }
+
+
+class _ReducerRegistry {
+    private  _reducersArray:{ <A extends Action>(state:Store<GurpsDataStore>,action:A):GurpsDataStore}[] = [];
+    public register(reducer) {
+        this._reducersArray.push(reducer);
+    }
+
+    public get reducers():any {
+        return this._reducersArray;
+    }
+
+}
+
+export const ReducerRegistry = new _ReducerRegistry();
+ReducerRegistry.register(AttributeReducer);
+
+// export function RegisterReducer() {
+//     return function(target,properyKey:string,decriptor: PropertyDescriptor) {
+//         ReducerRegistry.register(target);
+//     }
+// }
--- src/reducers/attribute-reducer.ts
+++ src/reducers/attribute-reducer.ts
@@ -4,8 +4,9 @@
 import {GurpsDataStore, GurpsDataStoreInitialState} from "../datastore/gurps-char-gen-datastore";
 import {AttributeAction, AttributeActionTypesConsts} from "../actions/attribute-action-factory";
 import * as deepAssign from "deep-assign";
+//import {ReducerRegistry, RegisterReducer} from "./aggregate-reducer";
 
-
+//@RegisterReducer
 export const AttributeReducer = (state: GurpsDataStore = GurpsDataStoreInitialState, action: AttributeAction): GurpsDataStore => {
     console.log("AttributeReducer."+action.type+"("+JSON.stringify(action)+")");
     let singleAttribute : any =  {};
@@ -27,3 +28,27 @@ export const AttributeReducer = (state: GurpsDataStore = GurpsDataStoreInitialSt
     }
 }
 
+//ReducerRegistry.register(AttributeReducer);
+
+// export const AttributeReducer = (state: GurpsDataStore = GurpsDataStoreInitialState, action: AttributeAction): GurpsDataStore => {
+//     console.log("AttributeReducer."+action.type+"("+JSON.stringify(action)+")");
+//     let singleAttribute : any =  {};
+//     switch (action.type) {
+//         case AttributeActionTypesConsts.SET_PRIMARY_ATTRIBUTE:
+//             singleAttribute[action.attributeId] = {value:action.value};
+//             return deepAssign({},state, {character:{attributes:singleAttribute}});
+//         case AttributeActionTypesConsts.SET_DERIVED_ATTRIBUTE:
+//             let attributeSystemData = state.system.attributes[action.attributeId];
+//             let costInput = action.cost;
+//             let trueCost = (costInput>0?
+//                     Math.ceil(costInput/attributeSystemData.costPerRaise):
+//                     Math.floor(costInput/attributeSystemData.costPerRaise))
+//                 *attributeSystemData.costPerRaise;
+//             singleAttribute[action.attributeId] = {cost:trueCost};
+//             return deepAssign({},state,{character:{attributes:singleAttribute}});
+//         default:
+//             return state;
+//     }
+// }
+
+// ReducerRegistry.register(AttributeReducer);
